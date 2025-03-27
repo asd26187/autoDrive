b2ee0a58322a140cb0232033f1471a150a6c9996
xiaoyun <kaeersnow@sina.com>
2025-03-27 15:01:12 +0800

    submit

---
+++ autoDrive/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  autoDrive
+//
+//  Created by iOS-wjc on 2025/3/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    var age = 2
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        self.age += 1
+    }
+
+
+}
+
